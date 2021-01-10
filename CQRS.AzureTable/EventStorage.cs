de75193830dbf447f0c1420df42bf143f77452fe
Chris Tacey-Green <chris@taceygreen.com>
2021-01-10 12:34:52 +0000

    Added AzureTableStorage implementation

---
+++ CQRS.AzureTable/EventStorage.cs
@@ -0,0 +1,63 @@
+﻿using CQRS.Events;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CQRS.AzureTable
+{
+    public class AzureTableEventStorage : IEventStorage
+    {
+        private readonly ILogger<AzureTableEventStorage> _log;
+        private readonly string _tableName;
+        private readonly CloudTableClient _tableClient;
+        public AzureTableEventStorage(ILogger<AzureTableEventStorage> log, string connectionStringVariableName, string tableName)
+        {
+            _log = log;
+            _tableName = tableName;
+            string storageConnectionString = Environment.GetEnvironmentVariable(connectionStringVariableName);
+
+            // Retrieve storage account information from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            // Create a table client for interacting with the table service
+            _tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+        }
+        public async Task<List<Event>> GetEventsForAggregate(Guid aggregateId)
+        {
+            CloudTable table = _tableClient.GetTableReference(_tableName);
+            TableContinuationToken token = null;
+            var eventsSegment = await table
+                .ExecuteQuerySegmentedAsync<TableEntityAdapter<Event>>(
+                    new TableQuery<TableEntityAdapter<Event>>()
+                    .Where(
+                        TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, aggregateId.ToString())), 
+                    token);
+            //TODO: deal with more than 1000 results by using continuation token
+            List<Event> events = new List<Event>();
+            foreach(var evt in eventsSegment)
+            {
+                events.Add(evt.OriginalEntity);
+            }
+            return events;
+        }
+
+        public Task<List<Event>> GetEventsSince(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveNewEventsForAggregate(Guid aggregateId, List<Event> newEvents)
+        {
+            // Create the batch operation. 
+            CloudTable table = _tableClient.GetTableReference(_tableName);
+            TableBatchOperation batchOperation = new TableBatchOperation();
+            foreach(var evt in newEvents)
+            {
+                batchOperation.Insert(new TableEntityAdapter<Event>(evt, evt.AggregateId.ToString(), evt.Id));
+            }
+            await table.ExecuteBatchAsync(batchOperation);
+        }
+    }
+}
---
+++ CQRS.AzureTable/ProjectionStore.cs
@@ -0,0 +1,41 @@
+﻿using CQRS.Projections;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.AzureTable
+{
+    public class AzureTableProjectionStore<T> : IProjectionStore<T> where T : Projection
+    {
+        private readonly ILogger<AzureTableProjectionStore<T>> _log;
+        private readonly string _tableName;
+        private readonly CloudTableClient _tableClient;
+        public AzureTableProjectionStore(ILogger<AzureTableProjectionStore<T>> log, string connectionStringVariableName, string tableName)
+        {
+            _log = log;
+            _tableName = tableName;
+            string storageConnectionString = Environment.GetEnvironmentVariable(connectionStringVariableName);
+
+            // Retrieve storage account information from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            // Create a table client for interacting with the table service
+            _tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+        }
+        public async Task<T> Get(string key)
+        {
+            CloudTable table = _tableClient.GetTableReference(_tableName);
+            var profileAdapter = await table.ExecuteAsync(TableOperation.Retrieve<TableEntityAdapter<T>>("", key));
+            return profileAdapter.Result as T;
+        }
+
+        public async Task Save(T projection, string key)
+        {
+            CloudTable table = _tableClient.GetTableReference(_tableName);
+            await table.ExecuteAsync(TableOperation.InsertOrMerge(new TableEntityAdapter<T>(projection, "", key)));
+        }
+    }
+}
